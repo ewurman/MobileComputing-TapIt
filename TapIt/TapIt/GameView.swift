29866dafc3b1dd0774fe3eeda87a365dd49bc6e3
Jason Nawrocki <jason_nawrocki@dhcp-215-216.wireless.bowdoin.edu>
2016-12-04 19:17:49 -0500

    test commit

--- TapIt/TapIt/GameView.swift
+++ TapIt/TapIt/GameView.swift
@@ -14,7 +14,7 @@ class GameView: UIView {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        // Drawing code...
     }
     */
 
